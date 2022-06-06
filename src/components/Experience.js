8b38c061ef35aac188d31223f900b2ff81377780
Jorge Navarro <53827082+jorgeebn16@users.noreply.github.com>
2022-06-06 15:00:48 -0400

    June 2022 1st Update
    June 2022 1st Update

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,9 +12,9 @@ const Experience = () => {
                     <div className= "timeline-content">
                         <h3>2010-present</h3>
                         <h2>it manager - inter bio-lab, inc.</h2>
-                        <p>Design, develop, implement, and coordinate company technology system policies, and procedures. Spearhead all aspects of IT department, comprising web designing, 
-                        and database development. Plan, organize, control, and oversee IT and electronic data operations. Act in alignment with user requirements and system functionality, 
-                        contributing to organizational policy.</p>
+                        <p>Design, develop, implement, and coordinate company technology system policies, and procedures. Spearhead all aspects of IT department, 
+                        comprising web designing, and database development. Plan, organize, control, and oversee IT and electronic data operations. Act in alignment 
+                        with user requirements and system functionality, contributing to organizational policy. </p>
                         <p>• Managed Server Management and Server Administration including Control Access and User Domain Control. </p>
                         <p>• Identified potential issues and provided effective solutions.</p>
                     </div>
@@ -25,9 +25,9 @@ const Experience = () => {
                     <div className= "timeline-content">
                         <h3>2010-present</h3>
                         <h2>e-commerce representative - inter bio-lab, inc.</h2>
-                        <p>Direct all facets of organization consisting of publishing, products and sales management as well as customer interaction through multiple platforms including eBay, 
-                        Amazon, DotMed, LabX, Medwow, and Kitmondo. Deliver knowledgeable answers to customer phone calls and e-mail inquiries regarding products and brand. 
-                        npmForge collaborative and positive working relationships with team members, clients, and senior management.  </p>
+                        <p>Direct all facets of the organization consisting of publishing, products and sales management as well as customer interaction through multiple 
+                        platforms including eBay, Amazon, DotMed, LabX, Medwow, and Kitmondo. Deliver knowledgeable answers to customer phone calls and e-mail inquiries regarding 
+                        products and brands. Forge collaborative and positive working relationships with team members, clients, and senior management.</p>
                         <p>• Design of advertising documents and product promotions. </p>
                         <p>• Followed-up on all customer requests to ensure the brand provides highest level of customer service. .</p>
                         <p>• Utilized CRM tools to manage ongoing client relationship.</p>
@@ -40,8 +40,9 @@ const Experience = () => {
                     <div className= "timeline-content">
                         <h3>2012-2018</h3>
                         <h2>ISO Internal Auditor - inter bio-lab, inc.</h2>
-                        <p>Generation and publishing of Internal Audit Calendar and carried out regular internal audits based on the published calendar. Ensured audit cycle program remained effective and upgraded. 
-                        Oversaw audit outcome and performed preventive/corrective actions. Verify that the solution proposed to resolve the preventive or corrective actions was appropriated and implemented. 
+                        <p>Generation and publishing of Internal Audit Calendar and carried out regular internal audits based on the published calendar. 
+                        Ensured audit cycle program remained effective and upgraded. Oversaw audit outcome and performed preventive/corrective actions. 
+                        Verify that the solution proposed to resolve the preventive or corrective actions was appropriated and implemented. 
                         Meet with the external Auditor and present the result of the internal audit. Produced and submitted a full audit report to the management team.</p>
                     </div>
                 </div>
@@ -50,7 +51,7 @@ const Experience = () => {
                     <div className="marker"></div>
                     <div className= "timeline-content">
                         <h3>2007-2009</h3>
-                        <h2>Quality Assurance Analyst, BOD/Corp Banca, Caracas-Venezuela</h2>
+                        <h2>Software Quality Assurance Analyst, BOD/Corp Banca, Caracas-Venezuela</h2>
                         <p>Responsible for Quality Assurance testing of new technological developments of the online banking platform (CorpLine), ATM and Point of Sales. 
                         As well testings in the banking platform of daily use in bank branches based on IBM Core Banking AS / 400 platform.</p>
                     </div>
